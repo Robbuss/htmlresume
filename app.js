5895d195c21a30999295bea4549401caef0a1ee8
Robbuss <vincentvanmontfoort@gmail.com>
2019-12-04 22:23:50 +0100

    initial

---
+++ app.js
@@ -0,0 +1,30 @@
+const mammoth = require('mammoth');
+const pdfparse = require('pdf-parse');
+const fs = require('fs');
+const path = require('path');
+
+(async function main() {
+    const resumedir = __dirname + '\\cvs'
+    let dirs = fs.readdirSync(resumedir)
+    dirs.splice(dirs.indexOf('.gitignore'), 1);
+
+    for (let i = 0; i < dirs.length; i++) {
+        const file = resumedir + '\\' + dirs[i];
+        // convert the docx files
+        if (file.endsWith('.docx') || file.endsWith('.doc')) {
+            const html = await mammoth.convertToHtml({ path: file });
+            fs.writeFile(file + ".html", html.value, function (err) {
+                if (err) throw err;
+                console.log('Created file.');
+            });
+        }
+        // convert the pdfs to text
+        if (file.endsWith('.pdf')) {
+            const pdf = await pdfparse(fs.readFileSync(file))
+            fs.writeFile(file + ".html", pdf.text, function (err) {
+                if (err) throw err;
+                console.log('Created file.');
+            });
+        }
+    }
+})();
