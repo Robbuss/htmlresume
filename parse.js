4e85eb80af691617c29a809a5e06b1377abb1d4d
Robbuss <vincentvanmontfoort@gmail.com>
2019-12-12 22:37:39 +0100

    Recursive dir search, word2pdf => pdf2text

--- parse.js
+++ parse.js
@@ -1,30 +1,124 @@
-const mammoth = require('mammoth');
-const pdfparse = require('pdf-parse');
 const fs = require('fs');
 const path = require('path');
 
-(async function main() {
-    const resumedir = __dirname + '\\cvs'
-    let dirs = fs.readdirSync(resumedir)
-    dirs.splice(dirs.indexOf('.gitignore'), 1);
+const pdfparse = require('pdf-parse');
+// const PDFParser = require("pdf2json");
+const word2pdf = require("word2pdf");
 
-    for (let i = 0; i < dirs.length; i++) {
-        const file = resumedir + '\\' + dirs[i];
-        // convert the docx files
-        if (file.endsWith('.docx') || file.endsWith('.doc')) {
-            const html = await mammoth.extractRawText({ path: file });
-            fs.writeFile(file + ".txt", html.value, function(err) {
-                if (err) throw err;
-                console.log('Created file.');
-            });
-        }
-        // convert the pdfs to text
-        if (file.endsWith('.pdf')) {
-            const pdf = await pdfparse(fs.readFileSync(file))
-            fs.writeFile(file + ".txt", pdf.text, function(err) {
-                if (err) throw err;
-                console.log('Created file.');
-            });
-        }
-    }
+const resumedir = __dirname + '\\cvs';
+const outputdir = resumedir + '\\output';
+
+(async function main() {
+    await wordToPdf(getFiles('doc'))
+    await pdfToText(getFiles('pdf'));
+    await toOutput();
 })();
+
+function getFiles(ext) {
+    let files = walk(resumedir)
+    let filtered = files.filter(x => x.includes('.' + ext))
+
+    return filtered;
+}
+
+function walk(dir) {
+    var results = [];
+    var list = fs.readdirSync(dir);
+    list.forEach(function(file) {
+        file = dir + '/' + file;
+        var stat = fs.statSync(file);
+        if (stat && stat.isDirectory()) { 
+            /* Recurse into a subdirectory */
+            results = results.concat(walk(file));
+        } else { 
+            /* Is a file */
+            results.push(file);
+        }
+    });
+    console.log(results.length + ' files.')
+    return results;
+}
+
+async function wordToPdf(files) {
+    for (let i = 0; i < files.length; i++) {
+
+        if (!files[i].match(/(\.docx|\.doc)/)) {
+            console.log('File is not a doc or docx, skipping...')
+            continue;
+        }
+
+        if (fs.existsSync(files[i].replace(/(docx|doc)/, 'pdf'))) {
+            console.log('PDF file exists, skipping..')
+            continue;
+        }
+
+        // convert doc to pdf
+        const data = await word2pdf(files[i])
+
+        fs.writeFileSync(files[i].replace(/(docx|doc)/, 'pdf'), data, function (err) {
+            console.log(err)
+        });
+        console.log('Converted ' + files[i] + ' to pdf');
+    }
+};
+
+async function pdfToText(files) {
+    for (let i = 0; i < files.length; i++) {
+
+        if (!files[i].endsWith('.pdf')) {
+            console.log('File is not a PDF, skipping...')
+            continue;
+        }
+        if (fs.existsSync(files[i].replace('pdf', 'txt').replace(path.dirname(files[i]), outputdir))) {
+            console.log('Text file exists, skipping..')
+            continue;
+        }
+        // console.log(fs.readFileSync(files[i]))
+        let pdf = await pdfparse(fs.readFileSync(files[i]))
+
+        fs.writeFile(files[i].replace('pdf', 'txt'), pdf.text, function (err) {
+            if (err) throw err;
+            console.log('Created file.');
+        });
+        console.log('Converted ' + files[i] + ' to text');
+    }
+}
+
+async function toOutput(){
+    let files = getFiles('txt')
+    let count = 0
+
+    for(let i = 0; i < files.length; i++){
+        let newPath = files[i].replace(path.dirname(files[i]), outputdir)
+
+        if (fs.existsSync(newPath)) {
+            console.log('Text file exists in output, skipping..')
+            continue;
+        }
+        // move the file by renaming the path
+        fs.rename(files[i], newPath, (err) => {
+            if (err) throw err;
+        });
+        count++;
+    }
+    console.log('Moved ' + count + ' files')
+}
+
+// async function pdfToJson(files) {
+//     let pdfParser = new PDFParser();
+//     for (let i = 0; i < files.length; i++) {
+//         const file = files[i];
+//         if (file.endsWith('.pdf')) {
+//             pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError));
+//             pdfParser.on("pdfParser_dataReady", pdfData => {
+//                 // console.log(JSON.stringify(pdfData));
+//                 fs.writeFile(file + ".json", JSON.stringify(pdfData), function (err) {
+//                     console.log('a')
+//                 });
+//             });
+
+//             pdfParser.loadPDF(file);
+//             console.log('Parsed ' + file);
+//         }
+//     }
+// };
